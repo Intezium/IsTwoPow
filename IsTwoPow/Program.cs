c7be438df01ce7bfbb49248db3e960dd0e0e7402
Intezium <Intezium04@gmail.com>
2018-03-17 17:03:06 +0300

    Проект завершен

--- IsTwoPow/Program.cs
+++ IsTwoPow/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IsTwoPow
 {
@@ -10,6 +6,39 @@ namespace IsTwoPow
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.Clear();
+
+                Console.WriteLine("===>[Проверка на степень двойки]<===");
+                Console.Write("\nВведите число: ");
+
+                try
+                {
+                    int number = int.Parse(Console.ReadLine());
+
+                    IsTwoPow(number);
+                }
+                catch (FormatException) { Console.WriteLine("\nНекорректное число!"); }
+
+                Console.WriteLine("\nEnter - Проверить еще");
+                Console.WriteLine("Esc - Выйти");
+
+                ConsoleKey inputKey = Console.ReadKey().Key;
+
+                if (inputKey == ConsoleKey.Enter)
+                    continue;
+                else if (inputKey == ConsoleKey.Escape)
+                    break;
+            }
+        }
+
+        static void IsTwoPow(int number)
+        {
+            if (number != 0 && ((number) & (number - 1)) == 0)
+                Console.WriteLine("\nЧисло {0} степень двойки", number);
+            else
+                Console.WriteLine("\nЧисло {0} не степень двойки", number);
         }
     }
 }
